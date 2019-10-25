1c9e29c1e258151c43e5e4ae5f667e49d4bace44
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-25 21:54:26 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,6 @@ require 'pry'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+
+Its for ".find_or_create_by_name", where its supposed to "invokes .create instead of re-coding the same functionality". the problem is I checked and .create is being used
